0cb48fd8b4de8c12604af9673f4c5bea564f59f7
Miguel <migapereira2005@gmail.com>
2025-02-26 12:56:53 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,8 +3,11 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld() {
+        lblMensagem2 = new JLabel("O Miguel Pereira esteve aqui");
+        panel1.add(lblMensagem2); // Adicionar lblMensagem2 ao panel1
         setContentPane(panel1);
         pack();
     }
